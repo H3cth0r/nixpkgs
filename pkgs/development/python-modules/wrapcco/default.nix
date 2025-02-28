fc0b9b69fad92872a67ddecd3a17eb3184413253
h3cth0r <hectorsucre13@gmail.com>
2025-02-27 18:13:06 -0600

    Address review feedback: fixed styling and correct keywords for package

--- pkgs/development/python-modules/wrapcco/default.nix
+++ pkgs/development/python-modules/wrapcco/default.nix
@@ -8,20 +8,20 @@
 buildPythonPackage rec {
   pname = "wrapcco";
   version = "0.1.3";  # Update version
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-nts5cujixB+PfAf9Fk7kOXW4BeI/0q5GYTthCI76ZOA=";
+    hash = "sha256-nts5cujixB+PfAf9Fk7kOXW4BeI/0q5GYTthCI76ZOA=";
   };
 
-  nativeBuildInputs = [ setuptools ];  # Build-time only
-  propagatedBuildInputs = [ numpy setuptools ];   # Remove setuptools here unless explicitly needed at runtime
+  build-system = [ setuptools ];  # Build-time only
+  dependencies = [ numpy setuptools ];   # Remove setuptools here unless explicitly needed at runtime
 
-  meta = with lib; {
-    description = "Supercharge Python with C++ extensions!...";
+  meta = {
+    description = "Supercharge Python with C++ extensions!";
     homepage = "https://github.com/H3cth0r/wrapc.co";
-    license = licenses.mit;
-    maintainers = with maintainers; [ h3cth0r ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ h3cth0r ];
   };
 }
